e2a0cfcc3117ba3cc338ba400678be4d3b4176c8
Zackary Richards <zackaryr@terpmail.umd.edu>
2020-05-06 13:48:48 -0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -57,7 +57,7 @@ const mymap = L.map('mapid').setView([38.9869, -76.9426], 15);
       fetch('/api') // Rather than fetching from PG County, we're going to fetch from our own server.
       .then(res => res.json())
       .then((data) => {
-        console.log(data.CrimeType)
+        console.log(data)
         for(i = 0; i < data.length; i++) {
           if (data[i].CrimeType === 'THEFT FROM AUTO') {
             theft_from_auto.push([data[i].latitude, data[i].longitude])
