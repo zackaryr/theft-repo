86d4474bdcefefb09ee1030f1ec93dfcf2773880
Zackary Richards <zackaryr@terpmail.umd.edu>
2020-05-05 20:12:00 -0400

    server update

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const fetch = require('node-fetch');
+import express from "express";
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
