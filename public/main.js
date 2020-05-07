7ab58843372afd610e29e97d251d85d085a3e329
Zackary Richards <zackaryr@terpmail.umd.edu>
2020-05-06 20:41:36 -0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -60,7 +60,7 @@ const mymap = L.map('mapid').setView([38.9869, -76.9426], 15);
         return data
       })
       .then((data) => {
-        console.log(typeof data);
+        console.log(Array.isArray(data));
         for (i = 0; i < data.length; i++) {
           if (data[i].CrimeType === 'THEFT FROM AUTO') {
             theft_from_auto.push([data[i].Latitude, data[i].Longitude])
